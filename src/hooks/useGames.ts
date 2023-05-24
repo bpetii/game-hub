c075ade62d9841e542191da516458a1ebdcc89cc
Peter Biro <biro.peti09@gmail.com>
2023-05-24 14:44:09 +0200

    Add emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,16 +3,17 @@ import useData from "./useData";
 
 
 export interface Platform {
-	id: number,
-	name: string,
-	slug: string
+	id: number;
+	name: string;
+	slug: string;
 }
 export interface Game {
-	id: number,
-	name: string,
-	background_image: string,
-	parent_platforms: {platform: Platform}[],
-	metacritic: number
+	id: number;
+	name: string;
+	background_image: string;
+	parent_platforms: {platform: Platform}[];
+	metacritic: number;
+	rating_top: number;
 }
 
 const useGames = (gameQuery: GameQuery) => 
